de933a2ebcf6e9dfda78e9728f1eb29b18db53bc
Osama Haj Ali <ossama.nae12@gmail.com>
2023-06-27 18:55:21 +0300

    Files Clean Up

--- app/components/InfluencersCollaborations.js
+++ app/components/InfluencersCollaborations.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "../styles/Services.module.scss";
 import Link from "next/link";
-import ImagesSlider from "./ImagesSlider";
 
 const InfluencersCollaborations = () => {
   return (
